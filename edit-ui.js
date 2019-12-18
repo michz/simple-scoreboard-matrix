e1c3469dad7f433cf19568062ca48b2814aa4a3c
Michael Zapf <m.zapf@mztx.de>
2019-12-18 09:56:20 +0100

    add very first tyble in electron only

---
+++ edit-ui.js
@@ -0,0 +1,74 @@
+//$('#scoreBoardTitle').html('TEST');
+
+var teams = [];
+
+var countColumns = function () {
+    return $('#scoreBoardHead tr:first th').length - 2;
+};
+
+var addRow = function () {
+    var rowContent = '';
+    var columnCount = countColumns();
+    for (var i = 0; i < columnCount; i++) {
+        rowContent += '<td><input class="result"></td>';
+    }
+
+    var row = '<tr>' +
+        '<td class="row-header"><input class="teamName" value="Team"></td>' +
+        rowContent +
+        '<td class="sum"></td>' +
+        '</tr>';
+
+    $('#scoreBoardBody').append(row);
+};
+
+var addColumn = function () {
+    var column = '<td><input class="result"></td>';
+    $('#scoreBoardHead tr th:last').before('<th>' + (countColumns() + 1) + '</th>');
+
+    $('#scoreBoardBody tr').each(function (idx, element) {
+        $(element).find('td:last').before(column);
+    });
+};
+
+var updateSums = function () {
+    $('#scoreBoardBody tr').each(function (idx, element) {
+        var sum = 0;
+        $(element).find('.result').each(function (idx, resultElement) {
+            var value = parseFloat($(resultElement).val());
+            if (false === isNaN(value))  {
+                sum += value;
+            }
+        });
+        $(element).find('.sum').text(sum);
+    });
+};
+
+var valueChanged = function (e) {
+    updateSums();
+};
+
+
+$('#btnAddRow').on('click', addRow);
+$('#btnAddColumn').on('click', addColumn);
+
+$('#scoreBoard').on('change', 'input', valueChanged);
+
+// Start without loaded data
+
+
+addColumn();
+addColumn();
+addColumn();
+addColumn();
+addRow();
+addRow();
+
+updateSums();
+
+
+/*
+TODO:
+* Sortieren
+* Daten mit Backend austauschen
+ */
